0c308a99d7acbb430339690cad8d71cd57b53a9e
Henry Anderson <handerson5486@gmail.com>
2020-04-22 14:21:40 -0700

    initial push

---
+++ DiabetesCalculatorv2/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  DiabetesCalculatorv2
+//
+//  Created by Nicole Miller on 4/17/20.
+//  Copyright © 2020 millihenry. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController{
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
